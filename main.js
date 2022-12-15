3d9ed7341dc55935793572a4d13774431e664c8d
Keandrrre <k.kuzniar@share.epsb.ca>
2022-12-15 13:28:07 -0700

    dfas

--- main.js
+++ main.js
@@ -1,34 +1,18 @@
-// Improved Task Manager
+const draggableElement = document.querySelector("#draggableEl");
 
-// Html Elements
+draggableElement.addEventListener("dragstart", (e) => {
+  e.dataTransfer.setData("text/plain", draggableElement.id);
+});
 
-// Global Variables
-let tasks = loadTasks();
+for (const dropZone of document.querySelectorAll(".container-dropzone")) {
+  // When draggable element is over a drop zone
+  dropZone.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropZone.classList.add("drop-zone--over");
+  });
 
-// Add New Task Button
-
-document.getElementById("newTaskBtn").addEventListener("click", addNewTask);
-
-function addNewTask() {
-  let newTask = prompt("Task Description:");
-  tasks.push(defineTaskStatus(newTask));
-  saveTasks();
-}
-
-// Helper Functions
-
-function defineTaskStatus(task) {
-  return {
-    description: task,
-    status: notcomplete,
-  };
-}
-
-function saveTasks() {
-  localStorage.setITem("tasks", JSON.stringify(tasks));
-}
-
-function loadTasks() {
-  let tasksStr = localStorage.getItem("tasks");
-  return JSON.parse(tasksStr) ?? [];
+  // When draggable element is dropped onto drop zone
+  dropZone.addEventListener("drop", (e) => {
+    e.preventDefault();
+  });
 }
